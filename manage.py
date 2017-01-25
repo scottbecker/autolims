d9b7b3fecf5358317e5cf9689f2753fb5d9459b4
Scott Becker <scott@delvetx.com>
2017-01-25 01:10:38 +0000

    temp changes for fast testing

--- manage.py
+++ manage.py
@@ -2,7 +2,17 @@
 import os
 import sys
 
+
+
 if __name__ == "__main__":
+    
+    sys.path.append("./autolims")
+    
+    print sys.path
+    
+    from lib import round_up    
+
+    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     try:
         from django.core.management import execute_from_command_line
--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,9 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-sys.path.append(os.path.join(BASE_DIR,"autolims"))
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
